a3b1c089457fe84fce5fe0aabfea61407fd6a08e
NYE-CODE <nikita@dspot.com.pl>
2025-05-05 21:29:20 +0300

    Added spaces

--- homework/nikita_yarmusik/homework_6/tasks.py
+++ homework/nikita_yarmusik/homework_6/tasks.py
@@ -6,9 +6,9 @@ updated_words = []
 
 for word in words:
     if word[-1] in ['.', ',']:
-        updated_words.append(word[0:-1]+'ing'+word[-1])
+        updated_words.append(word[0:-1] + 'ing' + word[-1])
     else:
-        updated_words.append(word+'ing')
+        updated_words.append(word + 'ing')
 
 print(' '.join(updated_words))
 
